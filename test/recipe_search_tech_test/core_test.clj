bef8fe8c4617284575f0b4ab1e069eea4d1bbf98
Bohdan Korniienko <bohdan.k@syte-vc.com>
2022-03-15 11:13:56 +0200

    Init project

---
+++ test/recipe_search_tech_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns recipe-search-tech-test.core-test
+  (:require [clojure.test :refer :all]
+            [recipe-search-tech-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
