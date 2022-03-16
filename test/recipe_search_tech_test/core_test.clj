027cfe52135c507ae8db762e1b7e087317777282
Bohdan Korniienko <bohdan.k@syte-vc.com>
2022-03-16 13:37:32 +0200

    rm tests folder

--- test/recipe_search_tech_test/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns recipe-search-tech-test.core-test
-  (:require [clojure.test :refer :all]
-            [recipe-search-tech-test.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
